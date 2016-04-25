bd45bd2fb8d61a241f4d44fe639b33814c814d60
fla91 <fabian.ladurner@students.fhv.at>
2016-04-25 21:02:44 +0200

    JavaFX importiert, startet jetzt über Main.java und öffnet die MainView.fxml

---
+++ src/Controller/MainViewController.java
@@ -0,0 +1,71 @@
+
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fabian Ladurner
+ */
+
+
+public class MainViewController implements Initializable {
+
+
+    @FXML
+    public AnchorPane anchorPaneContent;
+    @FXML
+    public ImageView imageCover;
+    
+    
+    // MENUs
+    @FXML
+    public AnchorPane paneMovieMenu;
+    
+    
+    
+    
+    
+    
+    @FXML
+    public void setPaneSelected(Event e) {
+        ((Pane) e.getSource()).getStyleClass().add("paneSelected");
+        ((Pane) e.getSource()).getChildren().get(0).getStyleClass().add("labelSelected");
+        paneMovieMenu.setVisible(true);
+    }
+    
+    public void setPaneUnselected(Event e) {
+        ((Pane) e.getSource()).getStyleClass().remove("paneSelected");
+        ((Pane) e.getSource()).getChildren().get(0).getStyleClass().remove("labelSelected");
+        paneMovieMenu.setVisible(false);
+    }
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        String imageSource = "https://image.tmdb.org/t/p/w396/i68IvNkUvqaKPY0UbadXcQ23aik.jpg";
+        
+        
+        imageCover.setImage(new Image(imageSource));
+        
+        System.out.println("immage");
+    }    
+    
+    
+    
+}

ba0f500f910c23cfbb8b9c36adaf0545536b44c6
fla91 <fabian.ladurner@students.fhv.at>
2016-04-26 09:42:04 +0200

    FXML erweiter

--- src/Controller/MovieContentController.java
+++ src/Controller/MovieContentController.java
@@ -2,7 +2,9 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -11,6 +13,11 @@ import javafx.fxml.Initializable;
  */
 public class MovieContentController implements Initializable {
 
+    @FXML
+    public TableView tableMovies;
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO: 
